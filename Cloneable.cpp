3ea18e4821d963ca623308d25d5e352675028877
Nicholas "LB" Braden <LB@LB-Stuff.com>
2014-08-15 01:58:42 -0500

    Also instantiate static data for Cloneable<std::shared_ptr>

--- Cloneable.cpp
+++ Cloneable.cpp
@@ -6,5 +6,7 @@ namespace resplunk
 	{
 		template<>
 		Cloneable<>::Registrar_t Cloneable<>::Implementor_t::registrar {};
+		template<>
+		Cloneable<std::shared_ptr>::Registrar_t Cloneable<std::shared_ptr>::Implementor_t::registrar {};
 	}
 }
